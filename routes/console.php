7eed87d580e403dceb568e065fea1dfd069f0671
Jitendra Kumar <jituthecoder@gmail.com>
2026-09-02 14:25:39 +0530

    Install and configure Laravel Telescope with strict 24-hour pruning and production error filtering

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule daily 24-hour automatic pruning of Telescope log entries to keep DB lightweight
+Schedule::command('telescope:prune --hours=24')->daily();
